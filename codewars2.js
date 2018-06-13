dd120b8e7cb3b79fd4c617e973ecf3e564079176
BrianArchibald <brianallenarchibald@gmail.com>
2018-06-12 20:08:51 -0500

    find smallest int, make num negative

--- codewars2.js
+++ codewars2.js
@@ -123,4 +123,32 @@ function removeChar(str){
 
 };
 
+/////////////////////////////////////////////////////
 
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Example:
+
+makeNegative(1); // return -1
+makeNegative(-5); // return -5
+makeNegative(0); // return 0
+
+
+function makeNegative(num) {
+  return num < 0 ? num : -num;
+}
+
+or 
+
+function makeNegative(num) {
+  return -Math.abs(num);
+
+///////////////////////////////////////////////////////
+
+Given an array of integers your solution should find the smallest integer.
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
