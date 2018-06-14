6662b124c0dac3ff01d4d4604cc7caa8a9f20285
BrianArchibald <brianallenarchibald@gmail.com>
2018-06-13 20:02:01 -0500

    reduce array but take out smallest/biggest int

--- codewars2.js
+++ codewars2.js
@@ -152,3 +152,33 @@ class SmallestIntegerFinder {
     return Math.min(...args)
   }
 }
+
+Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+Example:
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
+or 
+
+sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
+
+///////////////////////////////////////////////////////
+
