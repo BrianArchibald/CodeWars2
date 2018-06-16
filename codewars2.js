c1daa8ea2e8d6e733a3b9c51d475e1b78f07c19e
BrianArchibald <brianallenarchibald@gmail.com>
2018-06-15 20:04:20 -0500

    return numbers in reverse

--- codewars2.js
+++ codewars2.js
@@ -181,4 +181,18 @@ or
 sumArray = a => a ? a.sort((x, y) => x - y).slice(1, -1).reduce((s, e) => s + e, 0) : 0
 
 ///////////////////////////////////////////////////////
+Given a random number:
+
+C#: long;
+C++: unsigned long;
+You have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+
+function digitize(n){
+  return (n + '').split('').map(Number).reverse();
+}
+///////////////////////////////////////////////////////////////
+
 
