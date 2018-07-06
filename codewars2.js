e2d68b9e6249ed7e4fad3e6875193aa2bfe169e6
BrianArchibald <brianallenarchibald@gmail.com>
2018-07-05 19:18:30 -0500

    .find

--- codewars2.js
+++ codewars2.js
@@ -325,4 +325,34 @@ function confirmEnding(str, target) {
 
 //////////////////////////////////////////////
 
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test
 
+(second argument). If no element passes the test, return undefined.
+
+ 
+
+function findElement(arr, func) {
+  let num;
+  for( let i = 0; i < arr.length; i++) {
+   if(func(arr[i])) {
+      num = arr[i];
+      break;
+    }
+  }
+  return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+or
+
+The find() method returns the value of the first element
+
+in the array that satisfies the provided testing function.
+
+ Otherwise undefined is returned.
+
+
+return arr.find(func);
+ 
+
+///////////////////////////////
