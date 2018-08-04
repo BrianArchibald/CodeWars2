60a7bd5ac4762a7c483abf813be01dfe14b6d56a
BrianArchibald <brianallenarchibald@gmail.com>
2018-08-03 20:38:06 -0500

    string to num, square sum,

--- codewars2.js
+++ codewars2.js
@@ -720,3 +720,44 @@ let arr = [];
 
 /////////////////////////
 
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+var stringToNumber = function(str){
+  return Number(str);
+}
+
+/////////////////////////////
+
+Complete the squareSum/square_sum/SquareSum method so that it squares each number passed into it and then sums the results together.
+
+For example:
+
+squareSum([1, 2, 2]); // should return 9
+
+ 
+
+function squareSum(numbers){
+
+  return numbers.reduce((sum,num) => sum + (num * num), 0);
+  /////////////////////////////////////
+
+
+Create a function with two arguments that will return a list of length (n) with multiples of (x).
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+Return the results as an array (or list in Python, Haskell or Elixir).
+Examples:
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+
+ 
+function countBy(x, n) {
+
+    var z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+
+    //////////////////////////////
+
+    
