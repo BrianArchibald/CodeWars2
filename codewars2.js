2548545aa55e13abb778645fd411490470a2b590
BrianArchibald <brianallenarchibald@gmail.com>
2018-08-15 19:26:25 -0500

    added cat/dog years to reg years

--- codewars2.js
+++ codewars2.js
@@ -759,5 +759,69 @@ function countBy(x, n) {
     return z;
 
     //////////////////////////////
+I have a cat and a dog.
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+Return their respective ages now as [humanYears,catYears,dogYears]
+NOTES:
 
-    
+·        humanYears >= 1
+
+·        humanYears are whole numbers only
+
+·        15 cat years for first year
+
+·        +9 cat years for second year
+
+·        +4 cat years for each year after that
+
+Dog Years
+
+·        15 dog years for first year
+
+·        +9 dog years for second year
+
+·        +5 dog years for each year after that
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let catYears;
+  let dogYears;
+
+  if(humanYears === 1) {
+
+    catYears = 15;
+
+    dogYears = 15;
+
+  } else if (humanYears === 2) {
+
+    catYears = 24;
+
+    dogYears = 24;
+
+  } else {
+
+    catYears = 24 + ((humanYears - 2) * 4);
+
+    dogYears = 24 + ((humanYears - 2) * 5);
+
+  }
+
+  return [humanYears,catYears,dogYears];
+
+}
+
+Or
+const humanYearsCatYearsDogYears = humanYears => [
+
+  humanYears,
+
+  ( humanYears - 1 ? 16 : 11 ) + 4 * humanYears,
+
+  ( humanYears - 1 ? 14 : 10 ) + 5 * humanYears,
+
+];
+
+/////////////////////////////////////////////////
+
+   
