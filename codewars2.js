016498bb96433ae0fce7fd83f1be79ac0854b54a
BrianArchibald <brianallenarchibald@gmail.com>
2018-12-09 19:17:43 -0600

    fizzbuzz

--- codewars2.js
+++ codewars2.js
@@ -1042,3 +1042,17 @@ function rentalCarCost(d) {
 
 ////////////////////////////////////////
 
+Fizzbuzz
+
+Print numbers 1 to n, but "fizz" for multiples of 3, "buzz" for multiples of 5, and "fizzbuzz" for multiples of 15
+
+function fizzBuzz(n) {
+
+for (let i = 1; i <= n; i++) {
+
+if (i % 15 === 0) { console.log("fizzbuzz"); }
+else if (i % 5 === 0) { console.log("buzz"); }
+else if (i % 3 === 0) { console.log("fizz"); }
+else { console.log(i); } }
+
+}
